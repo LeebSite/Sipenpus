9f3b1d3a64992954552df65d30dc830f1d7e0662
LeebSite <mhd.ghalibpradipa@gmail.com>
2025-06-07 02:00:58 +0700

    membuat halaman loan utk member

---
+++ resources/views/filament/pages/member-loans.blade.php
@@ -0,0 +1,7 @@
+<x-filament-panels::page>
+    <div class="p-4 bg-white rounded shadow dark:bg-gray-800 dark:text-white">
+        <h1 class="text-2xl font-bold mb-6 dark:text-white">Riwayat Peminjaman Buku</h1>
+        
+        {{ $this->table }}
+    </div>
+</x-filament-panels::page>
