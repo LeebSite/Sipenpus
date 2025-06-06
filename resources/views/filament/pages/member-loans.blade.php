e841ad5f32665d3ac7ca247fb12df98caa1d52ab
LeebSite <mhd.ghalibpradipa@gmail.com>
2025-06-07 02:18:37 +0700

    perbaikan bug di Loan - Akses dan tampilan

--- resources/views/filament/pages/member-loans.blade.php
+++ resources/views/filament/pages/member-loans.blade.php
@@ -1,6 +1,6 @@
 <x-filament-panels::page>
     <div class="p-4 bg-white rounded shadow dark:bg-gray-800 dark:text-white">
-        <h1 class="text-2xl font-bold mb-6 dark:text-white">Riwayat Peminjaman Buku</h1>
+        <h1 class="text-2xl font-bold mb-4 dark:text-white">Riwayat Peminjaman Buku</h1>
         
         {{ $this->table }}
     </div>
---
+++ storage/framework/views/8a60fa3efa1f0df08c08bfbf662ae515.php
@@ -0,0 +1,25 @@
+<?php if (isset($component)) { $__componentOriginal166a02a7c5ef5a9331faf66fa665c256 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal166a02a7c5ef5a9331faf66fa665c256 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'filament-panels::components.page.index','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('filament-panels::page'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+    <div class="p-4 bg-white rounded shadow dark:bg-gray-800 dark:text-white">
+        <h1 class="text-2xl font-bold mb-4 dark:text-white">Employee Dashboard</h1>
+        <p class="dark:text-gray-300">Selamat datang, <?php echo e(auth()->user()->name); ?>!</p>
+    </div>
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal166a02a7c5ef5a9331faf66fa665c256)): ?>
+<?php $attributes = $__attributesOriginal166a02a7c5ef5a9331faf66fa665c256; ?>
+<?php unset($__attributesOriginal166a02a7c5ef5a9331faf66fa665c256); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal166a02a7c5ef5a9331faf66fa665c256)): ?>
+<?php $component = $__componentOriginal166a02a7c5ef5a9331faf66fa665c256; ?>
+<?php unset($__componentOriginal166a02a7c5ef5a9331faf66fa665c256); ?>
+<?php endif; ?>
+<?php /**PATH C:\laragon\www\sipenpus\resources\views/filament/pages/employee-dashboard.blade.php ENDPATH**/ ?>
