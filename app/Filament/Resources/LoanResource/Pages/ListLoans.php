d607ed6c44366470c5f603d3603187060b5c8b98
LeebSite <mhd.ghalibpradipa@gmail.com>
2025-06-07 01:59:12 +0700

    membuat list loans

---
+++ app/Filament/Resources/LoanResource/Pages/ListLoans.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\LoanResource\Pages;
+
+use App\Filament\Resources\LoanResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+use Illuminate\Support\Facades\Auth;
+
+class ListLoans extends ListRecords
+{
+    protected static string $resource = LoanResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        // Only admin and employee can create loans directly
+        if (Auth::user()->role === 'admin' || Auth::user()->role === 'employee') {
+            return [
+                Actions\CreateAction::make(),
+            ];
+        }
+        
+        return [];
+    }
+}
